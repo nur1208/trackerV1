283d45f73f283f6080a00cb982d9354a445d8377
nur <mode0o1208@yahoo.com>
2021-08-26 02:06:19 +0800

    building the UI

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @click="$emit(`toggle-add-task`)"
+      :text="isAddOpen ? `Close` : `Show Add Task`"
+      :color="isAddOpen ? `red` : `green`"
+    />
+  </header>
+</template>
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  //   props: ["title"],
+  props: { title: String, isAddOpen: Boolean },
+  //   props: { title: { type: String, default: "hello John" } },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+/* scoped mean this style only for this compo */
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <Task
+      @delete-task="onDelete(task.id)"
+      @toggle-reminder="$emit(`toggle-reminder`, task.id)"
+      :task="task"
+    />
+  </div>
+</template>
+<script>
+import Task from "./Task.vue";
+export default {
+  name: "Tasks",
+  props: { tasks: Array },
+  components: { Task },
+  methods: {
+    onDelete(id) {
+      this.$emit("delete-task", id);
+    },
+  },
+  emits: ["delete-task", `toggle-reminder`],
+};
+</script>
+<style lang=""></style>
